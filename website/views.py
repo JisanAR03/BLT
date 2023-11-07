62c53c7961f5b400be8e0661ded4a82442a29921
JisanAR03 <altafurrahaman3@gmail.com>
2023-11-08 02:52:10 +0600

    test

--- website/views.py
+++ website/views.py
@@ -818,14 +818,9 @@ class InviteCreate(TemplateView):
                 full_url_domain = "https://" + domain + "/favicon.ico"
                 if is_valid_https_url(full_url_domain):
                     safe_url = rebuild_safe_url(full_url_domain)
-                    try:
-                        response = requests.get(safe_url, timeout=5)
-                        if response.status_code == 200:
-                            exists = "exists"
-                        else:
-                            raise Exception
-                    except Exception as e:
-                        raise Exception
+                    response = requests.get(safe_url, timeout=5)
+                    if response.status_code == 200:
+                        exists = "exists"
             except:
                 pass
         context = {
