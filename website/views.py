315e19705261b6b63d5c3a56c1fbf0551d0a8d71
JisanAR03 <altafurrahaman3@gmail.com>
2023-11-12 02:00:29 +0600

    test

--- website/views.py
+++ website/views.py
@@ -756,8 +756,7 @@ class IssueCreate(IssueBaseCreate, CreateView):
                 return JsonResponse("Created", safe=False)
             else:
                 self.process_issue(self.request.user, obj, domain_exists, domain)
-                return HttpResponseRedirect(self.request.META.get("HTTP_REFERER"))
-                # return redirect("/report/")
+                return redirect(redirect_url)
         
         return create_issue(self,form)
 
